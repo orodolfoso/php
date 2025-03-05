26b5cfc234c6fa69da0d7b0c1dbf4bda1b4b5b16
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-05 20:41:18 -0300

    Operadores de Incremento/Decremento

--- public/index.php
+++ public/index.php
@@ -1,7 +1,45 @@
 <?php
 
+//Camelcase - MyName
+// snake_case - my_name
+
+$name = 'Rodolfo';
 
 
-require './teste.php';
+echo gettype($name); 
+echo "<br>";
 
-echo $name;
+// String
+
+echo gettype('Rodolfo');
+echo "<br>";
+
+// Numebrs - intergers, floats(duble)
+
+echo gettype(123);
+echo "<br>";
+echo gettype(123.4);
+echo "<br>";
+
+// Boleans
+
+echo gettype(true);
+echo "<br>";
+
+// Arrays
+
+echo gettype(['Rodollfo', 39, true]);
+echo "<br>";
+
+// Object
+
+class Person {
+
+}
+
+echo gettype(new Person);
+echo "<br>";
+
+// NULL
+
+echo gettype(null); 
--- public/teste.php
+++ public/teste.php
@@ -1,4 +1,21 @@
 <?php
 
-$name = 'Lucas';
+// $name = 'Rodolfo';
 
+
+// $name = $name . ' Oliveira';
+
+// echo $name;
+
+// $number = 10;
+
+// $number += 20;
+
+// echo $number;
+
+// Incremento/Decremento
+
+$number = 10;
+
+$number--;
+echo $number;
